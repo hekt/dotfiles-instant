d83619a0eec0b1c4ff2bbea48756c77d80e4f391
hekt <hektorg@gmail.com>
2017-07-09 11:13:03 +0900

    refactoring

--- scripts/install-git-contrib.sh
+++ scripts/install-git-contrib.sh
@@ -4,13 +4,12 @@ set -e
 set -u
 
 GIT_VERSION=`git --version | sed -e 's/^git version //'`
-GH_URL_1='https://raw.githubusercontent.com/git/git/v'
-GH_URL_2='/contrib/completion/'
+GH_URL="https://raw.githubusercontent.com/git/git/v${GIT_VERSION}/contrib/"
 
 if [ -e $HOME/.git-prompt.sh ]; then
     echo ".git-prompt.sh already exists"
 else
-    GIT_PROMPT_URL="${GH_URL_1}${GIT_VERSION}${GH_URL_2}git-prompt.sh"
+    GIT_PROMPT_URL="${GH_URL}completion/git-prompt.sh"
     echo "download from ${GIT_PROMPT_URL}"
     curl -fs $GIT_PROMPT_URL > $HOME/.git-prompt.sh
 fi
@@ -18,7 +17,7 @@ fi
 if [ -e $HOME/.git-completion.bash ]; then
     echo ".git-completion.bash already exists"
 else
-    GIT_COMPLETION_URL="${GH_URL_1}${GIT_VERSION}${GH_URL_2}git-completion.bash"
+    GIT_COMPLETION_URL="${GH_URL}completion/git-completion.bash"
     echo "download from ${GIT_COMPLETION_URL}"
     curl -fs $GIT_COMPLETION_URL > $HOME/.git-completion.bash
 fi
