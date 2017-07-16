872fdc117ebd1893e5c432d53f81d6d7cbb84823
hekt <hektorg@gmail.com>
2017-07-16 19:08:08 +0900

    fix initialize test

--- tests/initialize-test.sh
+++ tests/initialize-test.sh
@@ -7,5 +7,5 @@ set -x
 PARENT_DIR=$(cd $(dirname $0); cd ..; pwd)
 
 # git contribs
-test -f $PARENT_DIR/files/.git.d/contrib/git-prompt.sh
-test -f $PARENT_DIR/files/.git.d/contrib/git-completion.bash
+test -f $PARENT_DIR/files/.git.d/contrib/completion/git-prompt.sh
+test -f $PARENT_DIR/files/.git.d/contrib/completion/git-completion.bash
